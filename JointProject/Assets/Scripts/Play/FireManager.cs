625092848f7cca3c88718ecc20199991c6d6bef4
DESKTOP-FNDRUJQ\Daichi Muraoka <nianago0415@gmail.com>
2021-01-13 21:37:52 +0900

    各フォルダ作成、Playシーン追加

---
+++ JointProject/Assets/Scripts/Play/FireManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Bullet nomalBullet = null;
+
+    [SerializeField]
+    private Bullet flyBullet = null;
+
+    [SerializeField]
+    private KeyCode nomalFire = KeyCode.E;
+
+    [SerializeField]
+    private KeyCode flyFire = KeyCode.F;
+
+    private FIRE_TYPE fireType = FIRE_TYPE.DEFAULT;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(nomalFire))
+        {
+
+        }
+        if (Input.GetKeyDown(flyFire))
+        {
+
+        }
+    }
+
+    public virtual void Fire()
+    {
+
+    }
+}
+public enum FIRE_TYPE
+{
+    DEFAULT,
+    NOMAL,
+    FLY
+}
