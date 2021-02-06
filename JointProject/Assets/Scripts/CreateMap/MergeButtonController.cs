b396b2a0a2d6a2f29595c6434ca93dca3531944d
kanoko312 <kanokano0312@gmail.com>
2021-02-06 16:50:16 +0900

    全てのマップパーツが連続的に繋がっているか判定を追加。 「git rm -r JointProject/Library」で余計なキャッシュを削除。

--- JointProject/Assets/Scripts/CreateMap/MergeButtonController.cs
+++ JointProject/Assets/Scripts/CreateMap/MergeButtonController.cs
@@ -17,6 +17,9 @@ public class MergeButtonController : MonoBehaviour
     private BoxCollider[] colliders;
     private Vector3[] previousPositions; //前回位置
     private bool[] mapPartsEnable; //合成可能な状態か
+    private bool[] intrusion; //他パーツと重なっているか
+    private List<List<int>> connected = new List<List<int>>(); //繋がっているマップパーツのインデックスリスト
+    private bool[] appeared; //つながり判定用
     
     private Color disableColor = Color.gray; //無効時の色
     private Color enableColor = Color.white; //有効時の色
@@ -32,6 +35,8 @@ public class MergeButtonController : MonoBehaviour
         colliders = new BoxCollider[mapParts.Length];
         previousPositions = new Vector3[mapParts.Length];
         mapPartsEnable = new bool[mapParts.Length];
+        intrusion = new bool[mapParts.Length];
+        appeared = new bool[mapParts.Length];
         
         for(int i=0; i<mapParts.Length; i++){
             //ボックスコライダーを取得
@@ -40,6 +45,8 @@ public class MergeButtonController : MonoBehaviour
             previousPositions[i] = mapParts[i].transform.position;
             //最初は全て有効状態
             mapPartsEnable[i] = true;
+            //他パーツとは重なっていない
+            intrusion[i] = false;
             //MapPartsを無効に変更
             changeMapPartsEnablement(i, false);
         }
@@ -55,19 +62,23 @@ public class MergeButtonController : MonoBehaviour
         for(int i=0; i<mapParts.Length; i++){
             //マップパーツが移動したとき
             if(mapParts[i].transform.position != previousPositions[i]){
+                //侵入判定を更新
+                judgeMapPartsIntrusion(i);
+                
                 //MapPartsの有効判定
                 for(int j=0; j<mapParts.Length; j++)
                     judgeMapPartsAdjacent(j);
-                    
+                
                 //前回位置を更新
                 previousPositions[i] = mapParts[i].transform.position;
                 
-                //全てのマップパーツが有効のときボタンを有効にする
+                //全てのマップパーツが有効のとき
                 int enableSum = 0;
                 for(int j=0; j<mapParts.Length; j++)
                     if(mapPartsEnable[j]) enableSum++;
                 
-                if(enableSum == mapParts.Length)
+                //全てのマップパーツが繋がっているとき
+                if(enableSum == mapParts.Length && isConnectedAllMapParts(0))
                     changeButtonInteractable(true);
                 else
                     changeButtonInteractable(false);
@@ -79,6 +90,102 @@ public class MergeButtonController : MonoBehaviour
     }
     
     /// <summary>
+    /// マップパーツが全て繋がっているか再帰的に判定する。
+    /// 最初の呼び出し時の引数は0。
+    /// </summary>
+    /// <returns></returns>
+    private bool isConnectedAllMapParts(int selected)
+    {
+        //初期化
+        if(selected == 0){
+            for(int i=0; i<appeared.Length; i++)
+                appeared[i] = false;
+        }
+        
+        //探索済なら
+        if(appeared[selected] == true) return true;
+        
+        //selectedを探索済にする
+        appeared[selected] = true;
+        
+        //全部探索済なら打ち切り
+        int appearedCount = 0;
+        for(int i=0; i<appeared.Length; i++)
+                if(appeared[i] == true) appearedCount++;
+        if(appearedCount == appeared.Length) return true;
+        
+        //connectedがなくなったら打ち切り
+        if(connected.Count == 0) return false;
+        
+        //次の行き先をリストに追加
+        List<int> contactParts = new List<int>();
+        for(int i=0; i<connected.Count; i++){
+            if(connected[i][0] == selected){
+                if(appeared[connected[i][1]] == false) contactParts.Add(connected[i][1]);
+                connected.RemoveAt(i);
+            }
+            else if(connected[i][1] == selected){
+                if(appeared[connected[i][0]] == false) contactParts.Add(connected[i][0]);
+                connected.RemoveAt(i);
+            }
+        }
+        
+        //行き先がなければ打ち切り
+        if(contactParts.Count == 0) return false;
+        
+        //再帰探索
+        for(int i=0; i<contactParts.Count; i++){
+            bool dammy = isConnectedAllMapParts(contactParts[i]);
+        }
+        
+        if(selected == 0){
+            connected.Clear();
+            
+            for(int i=0; i<appeared.Length; i++)
+                Debug.Log("appeared["+i+"]:"+appeared[i]);
+            
+            for(int i=0; i<appeared.Length; i++)
+                if(appeared[i] == false)
+                    return false;
+        }
+        
+        return true;
+    }
+    
+    /// <summary>
+    /// 指定のマップパーツについて、他パーツとの重なりを判定し、intrusionを更新する
+    /// </summary>
+    private void judgeMapPartsIntrusion(int selected)
+    {
+        float errorValue = 0.3f; //許容される誤差
+        
+        Vector3 selectedPos = mapParts[selected].transform.position + colliders[selected].center;
+        Vector3 selectedSize = colliders[selected].size/2;
+        
+        intrusion[selected] = false;
+        for(int i=0; i<mapParts.Length; i++)
+        {
+            if(i == selected){
+                i++;
+            }else{
+                Vector3 otherPos = mapParts[i].transform.position + colliders[i].center;
+                Vector3 otherSize = colliders[i].size/2;
+                Vector3 MaxDistance = selectedSize + otherSize;
+                
+                float dx = Mathf.Abs(selectedPos.x - otherPos.x);
+                float dz = Mathf.Abs(selectedPos.z - otherPos.z);
+                
+                //侵入判定
+                if(dx < MaxDistance.x - errorValue && dz < MaxDistance.z - errorValue)
+                {
+                    intrusion[selected] = true;
+                    break;
+                }
+            }
+        }
+    }
+    
+    /// <summary>
     /// 指定のMapPartsが他のMapPartsと隣接していたら，隣接する2つのMapPartsを有効にする．
     /// 隣接しなければ無効にする．
     /// </summary>
@@ -86,13 +193,10 @@ public class MergeButtonController : MonoBehaviour
     private void judgeMapPartsAdjacent(int selected)
     {
         int contactBlockNum = 3; //最低隣接しなければならないブロック数
-        float errorValue = 0.5f; //許容される誤差
+        float errorValue = 0.3f; //許容される誤差
         
-        Vector3 selectedTop = mapParts[selected].transform.position + colliders[selected].center
-            + colliders[selected].size/2;
-        Vector3 selectedBottom = mapParts[selected].transform.position + colliders[selected].center
-            - colliders[selected].size/2;
-        Vector3 selectedSize = colliders[selected].size;
+        Vector3 selectedPos = mapParts[selected].transform.position + colliders[selected].center;
+        Vector3 selectedSize = colliders[selected].size/2;
         
         int sumAdjacent = 0; //隣接マップパーツ数カウンタ
         
@@ -101,32 +205,28 @@ public class MergeButtonController : MonoBehaviour
             if(i == selected){
                 i++;
             }else{
-                Vector3 otherTop = mapParts[i].transform.position + colliders[i].center
-                    + colliders[i].size/2;
-                Vector3 otherBottom = mapParts[i].transform.position + colliders[i].center
-                    - colliders[i].size/2;
-                Vector3 otherSize = colliders[i].size;
-                Vector3 MaxDistance = selectedSize + colliders[i].size;
-                MaxDistance += new Vector3(errorValue - contactBlockNum, errorValue - contactBlockNum, errorValue - contactBlockNum);
+                Vector3 otherPos = mapParts[i].transform.position + colliders[i].center;
+                Vector3 otherSize = colliders[i].size/2;
+                Vector3 MaxDistance = selectedSize + otherSize;
+                
+                float dx = Mathf.Abs(selectedPos.x - otherPos.x);
+                float dz = Mathf.Abs(selectedPos.z - otherPos.z);
                 
                 //隣接判定 
-                if( ((Mathf.Abs(selectedTop.z - otherBottom.z) < errorValue
-                    || Mathf.Abs(selectedBottom.z - otherTop.z) < errorValue) &&
-                        ((0 <= selectedTop.x - otherBottom.x && selectedTop.x - otherBottom.x < MaxDistance.x)
-                        && (0 <= otherTop.x - selectedBottom.x && otherTop.x - selectedBottom.x < MaxDistance.x)))
-                || 
-                    ((Mathf.Abs(selectedTop.x - otherBottom.x) < errorValue
-                    || Mathf.Abs(selectedBottom.x - otherTop.x) < errorValue) &&
-                        ((0 <= selectedTop.z - otherBottom.z && selectedTop.z - otherBottom.z < MaxDistance.z)
-                        && (0 <= otherTop.z - selectedBottom.z && otherTop.z - selectedBottom.z < MaxDistance.z))) )
+                if((Mathf.Abs(dx - MaxDistance.x) < errorValue && dz < MaxDistance.z - contactBlockNum + errorValue)
+                || (Mathf.Abs(dz - MaxDistance.z) < errorValue && dx < MaxDistance.x - contactBlockNum + errorValue) )
                 {
+                    if(intrusion[i] == false)
                         changeMapPartsEnablement(i, true); //Bottom側の判定が不安定なので念のため
-                        sumAdjacent++;
+                    connected.Add(new List<int>{selected, i}); //隣接している組み合わせを保存
+                    sumAdjacent++;
                 }
+                
             }
         }
         
-        if(sumAdjacent != 0){
+        //他パーツに侵入しておらず、隣接パーツがあるとき有効にする
+        if(intrusion[selected] == false && sumAdjacent != 0){
             changeMapPartsEnablement(selected, true);
         }else{
             changeMapPartsEnablement(selected, false);
