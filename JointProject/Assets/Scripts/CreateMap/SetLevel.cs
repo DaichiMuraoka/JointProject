9e0d9caf215c17ed3bb6453974ca32dab4cc6a11
kanoko312 <kanokano0312@gmail.com>
2021-02-08 21:38:54 +0900

    マップパーツの読み出し、合成したマップの書き出しを実装。マップパーツやレベルの作製。

---
+++ JointProject/Assets/Scripts/CreateMap/SetLevel.cs
@@ -0,0 +1,18 @@
+﻿//kanoko
+//スクリプタブルオブジェクトからレベルを読み出しシーンに配置する。
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetLevel : MonoBehaviour
+{
+    [SerializeField]
+    private MapDeliverer mapDeliverer = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(mapDeliverer.Map);
+    }
+}
