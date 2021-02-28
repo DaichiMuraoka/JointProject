583a248ea8a5f656642d414374fbbec158b52548
kanoko312 <kanokano0312@gmail.com>
2021-02-28 14:10:57 +0900

    タイトルシーンのアニメーション、シーン遷移などを実装。

---
+++ JointProject/Assets/Scripts/Title/ContinuousPlayAnimation.cs
@@ -0,0 +1,26 @@
+﻿//kanoko
+//インスペクターで指定したアニメーションを連続再生する。
+//AnimationコンポーネントのAnimationsにも指定してないと動かない。
+
+using UnityEngine;
+using System.Collections;
+
+public class ContinuousPlayAnimation : MonoBehaviour {
+
+    [SerializeField]
+    private AnimationClip[] clips = null;
+
+    private Animation anim;
+    
+
+    // Use this for initialization
+    void Start () {
+        anim = gameObject.GetComponent<Animation>();
+        anim.playAutomatically = false;
+        
+        foreach (AnimationClip clip in clips) {
+            anim.PlayQueued(clip.name, QueueMode.CompleteOthers);
+        }
+        
+    }
+}
---
+++ JointProject/Assets/Scripts/Title/TransitionNextScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionNextScene : MonoBehaviour
+{
+    [SerializeField] private GameObject fadeCurtain = null;
+    
+    private AudioSource audioSource;
+    private FadeTransition fadeTransition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        fadeTransition = fadeCurtain.GetComponent<FadeTransition>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            fadeTransition.StartFadeOut();
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+    }
+}
