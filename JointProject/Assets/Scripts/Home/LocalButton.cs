d78c1271b75d5f9353c49190a4ec9ee9bb44014a
kanoko312 <kanokano0312@gmail.com>
2021-03-02 09:27:04 +0900

    ホームシーンを実装。ローカルのUIを整えた。音楽ファイルの追加。

---
+++ JointProject/Assets/Scripts/Home/LocalButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocalButton : MonoBehaviour
+{
+    public void OnClick()
+    {
+        //シーン移動
+        SceneManager.LoadScene("Local");
+    }
+}
